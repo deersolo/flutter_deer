d62bef4129da1b12a25cbf8275f87eb7ff38a15b
Saksorn <suworapong@hotmail.co.th>
2023-03-07 15:32:06 +0700

    Test-2 App

--- lib/src/login/widgets/single_sign_on.dart
+++ lib/src/login/widgets/single_sign_on.dart
@@ -1,25 +1,26 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SingleSignOn extends StatelessWidget{
-  const SingleSignOn({super.key});
+class SingleSignOn extends StatelessWidget {
+  const SingleSignOn({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
       children: [
-        _buildDivider(),
+        _buildDivider()
       ],
     );
   }
 
   Row _buildDivider() {
-    const gradientColor = [Colors.white10, Colors.white];
-    line(List<Color> colors) => Container(
-      decoration: const BoxDecoration(
+    final gradientColor = const [Colors.white10, Colors.white];
+    final line = (List<Color> colors) => Container(
+      decoration:  BoxDecoration(
         gradient: LinearGradient(
           colors: colors,
-          begin: Alignment.centerLeft,
+          begin:  Alignment.centerLeft,
           end: Alignment.centerRight,
           stops: [0.0, 1.0],
         ),
@@ -27,18 +28,21 @@ class SingleSignOn extends StatelessWidget{
       width: 80.0,
       height: 1.0,
     );
+
     return Row(
     mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      line(gradientColor),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Text('or' , style:  TextStyle(color: Colors.white60, fontSize: 16.0),
-
+      children: [
+        line(gradientColor),
+        const Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 15),
+          child: Text ('or', style: TextStyle(
+            color: Colors.white60,
+            fontSize: 16.0,
+          ),
+          ),
         ),
-      ),
-      line(gradientColor.reversed.toList()),
-    ],
+        line(gradientColor.reversed.toList())
+    ]
   );
   }
 }
