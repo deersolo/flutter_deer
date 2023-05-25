a210bcf2a058c984d6375a71630b7cf209491dab
Saksorn <suworapong@hotmail.co.th>
2023-05-25 16:07:14 +0700

    Test-4 App

--- lib/src/pages/management/widgets/product_image.dart
+++ lib/src/pages/management/widgets/product_image.dart
@@ -1,7 +1,10 @@
+
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-//import 'package:image_picker/image_picker.dart';
+import 'package:image_picker/image_picker.dart';
+
 
 
 class ProductImage extends StatefulWidget {
@@ -12,8 +15,9 @@ class ProductImage extends StatefulWidget {
 }
 
 class _ProductImageState extends State<ProductImage> {
-  late File _imageFile = File('path');
-  //final _picker = ImagePicker();
+  final _picker = ImagePicker();
+  late File _imageFile;
+  String? check;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +27,7 @@ class _ProductImageState extends State<ProductImage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _buildPickerImage(),
-          //_buildPreviewImage(),
+          _buildPreviewImage(),
         ],
       ),
     );
@@ -31,91 +35,99 @@ class _ProductImageState extends State<ProductImage> {
 
   _buildPickerImage() => OutlinedButton.icon(
       onPressed: () {
-        //_modalPickerImage();
-        debugPrint('Received click test');
+        _modalPickerImage();
       },
       icon: FaIcon(FontAwesomeIcons.image),
       label: Text('image'));
 
-  dynamic _buildPreviewImage() {
-    if (_imageFile != 'path') {
+  dynamic _buildPreviewImage() {    
+    if (check == null) {
+      debugPrint('_imageFile = null');
       return SizedBox();
-    } else {
+    }
+    else
+    {
+      debugPrint('_imageFile = >> new path');
       final container = (Widget child) => Container(
-            color: Colors.grey[100],
-            margin: EdgeInsets.only(top: 4),
-            alignment: Alignment.center,
-            height: 350,
-            child: child,
-          );
+        color: Colors.grey[100],
+        margin: EdgeInsets.only(top: 4),
+        alignment: Alignment.center,
+        height: 350,
+        child: child,
+      );
+
 
       return Stack(
         children: [
           container(Image.file(_imageFile)),
           _buildDeleteImageButton(),
         ],
+
       );
     }
   }
 
   _buildDeleteImageButton() => Positioned(
-        right: 0,
-        child: IconButton(
-          onPressed: () {
-            //todo
-          },
-          icon: Icon(
-            Icons.clear,
-            color: Colors.black54,
-          ),
-        ),
-      );
+    right: 0,
+    child: IconButton(
+      onPressed: () {
+        //todo
+      },
+      icon: Icon(
+        Icons.clear,
+        color: Colors.black54,
+      ),
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+    ),
+  );
 
-  // void _modalPickerImage() {
-  //   final buildListTile =
-  //       (IconData icon, String title, ImageSource) => ListTile(
-  //             leading: Icon(icon),
-  //             title: Text(title),
-  //             onTap: () {
-  //               Navigator.pop(context);
-  //               _pickImages(ImageSource);
-  //             },
-  //           );
-  //
-  //   showModalBottomSheet(
-  //     context: context,
-  //     builder: (context) => Column(
-  //       mainAxisSize: MainAxisSize.min,
-  //       children: <Widget>[
-  //         buildListTile(
-  //           Icons.photo_camera,
-  //           "Take a picture from camera",
-  //           ImageSource.camera,
-  //         ),
-  //         buildListTile(
-  //           Icons.photo_library,
-  //           "Choose from photo library",
-  //           ImageSource.gallery,
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
-  //
-  // void _pickImages(ImageSource imageSource) {
-  //   _picker
-  //       .getImage(
-  //     source: imageSource,
-  //     imageQuality: 70,
-  //     maxHeight: 500,
-  //     maxWidth: 500,
-  //   )
-  //       .then((file) {
-  //     if (file != null) {
-  //       setState(() {
-  //         _imageFile = File(file.path);
-  //       });
-  //     }
-  //   });
-  // }
+void _modalPickerImage() {
+  final buildListTile =
+      (IconData icon, String title, ImageSource) => ListTile(
+            leading: Icon(icon),
+            title: Text(title),
+            onTap: () {
+              Navigator.pop(context);
+              _pickImages(ImageSource);
+            },
+          );
+
+  showModalBottomSheet(
+    context: context,
+    builder: (context) => Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        buildListTile(
+          Icons.photo_camera,
+          "Take a picture from camera",
+          ImageSource.camera,
+        ),
+        buildListTile(
+          Icons.photo_library,
+          "Choose from photo library",
+          ImageSource.gallery,
+        ),
+      ],
+    ),
+  );
+}
+
+void _pickImages(ImageSource imageSource) {
+  _picker
+      .getImage(
+    source: imageSource,
+    imageQuality: 70,
+    maxHeight: 500,
+    maxWidth: 500,
+  )
+      .then((file) {
+    if (file != null) {
+      setState(() {
+        _imageFile = File(file.path);
+        check = '1';
+      });
+    }
+  });
+}
 }
