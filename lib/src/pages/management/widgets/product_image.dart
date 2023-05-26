1dfd281507d37471cfe40373e5d0b57e4c705369
Saksorn <suworapong@hotmail.co.th>
2023-05-26 14:28:18 +0700

    Image_Cropper

--- lib/src/pages/management/widgets/product_image.dart
+++ lib/src/pages/management/widgets/product_image.dart
@@ -1,11 +1,8 @@
-
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
-
-
 
 class ProductImage extends StatefulWidget {
   const ProductImage({Key? key}) : super(key: key);
@@ -40,94 +37,117 @@ class _ProductImageState extends State<ProductImage> {
       icon: FaIcon(FontAwesomeIcons.image),
       label: Text('image'));
 
-  dynamic _buildPreviewImage() {    
+  dynamic _buildPreviewImage() {
     if (check == null) {
       debugPrint('_imageFile = null');
       return SizedBox();
-    }
-    else
-    {
+    } else {
       debugPrint('_imageFile = >> new path');
       final container = (Widget child) => Container(
-        color: Colors.grey[100],
-        margin: EdgeInsets.only(top: 4),
-        alignment: Alignment.center,
-        height: 350,
-        child: child,
-      );
-
+            color: Colors.grey[100],
+            margin: EdgeInsets.only(top: 4),
+            alignment: Alignment.center,
+            height: 350,
+            child: child,
+          );
 
       return Stack(
         children: [
           container(Image.file(_imageFile)),
           _buildDeleteImageButton(),
         ],
-
       );
     }
   }
 
   _buildDeleteImageButton() => Positioned(
-    right: 0,
-    child: IconButton(
-      onPressed: () {
-        //todo
-      },
-      icon: Icon(
-        Icons.clear,
-        color: Colors.black54,
-      ),
-      splashColor: Colors.transparent,
-      highlightColor: Colors.transparent,
-    ),
-  );
-
-void _modalPickerImage() {
-  final buildListTile =
-      (IconData icon, String title, ImageSource) => ListTile(
-            leading: Icon(icon),
-            title: Text(title),
-            onTap: () {
-              Navigator.pop(context);
-              _pickImages(ImageSource);
-            },
-          );
-
-  showModalBottomSheet(
-    context: context,
-    builder: (context) => Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        buildListTile(
-          Icons.photo_camera,
-          "Take a picture from camera",
-          ImageSource.camera,
+        right: 0,
+        child: IconButton(
+          onPressed: () {
+            //todo
+          },
+          icon: Icon(
+            Icons.clear,
+            color: Colors.black54,
+          ),
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
         ),
-        buildListTile(
-          Icons.photo_library,
-          "Choose from photo library",
-          ImageSource.gallery,
+      );
+
+  void _modalPickerImage() {
+    final buildListTile =
+        (IconData icon, String title, ImageSource) => ListTile(
+              leading: Icon(icon),
+              title: Text(title),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImages(ImageSource);
+                check = '1';
+              },
+            );
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          buildListTile(
+            Icons.photo_camera,
+            "Take a picture from camera",
+            ImageSource.camera,
+          ),
+          buildListTile(
+            Icons.photo_library,
+            "Choose from photo library",
+            ImageSource.gallery,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _pickImages(ImageSource imageSource) {
+    _picker
+        .getImage(
+      source: imageSource,
+      imageQuality: 70,
+      maxHeight: 500,
+      maxWidth: 500,
+    ).then((file) {
+      if (file != null) {
+        _cropImage(file.path);
+      }
+    });
+  }
+
+  void _cropImage(String file) {
+    ImageCropper().cropImage(
+      sourcePath: file,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9
+      ],
+      uiSettings: [
+        AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.blueGrey,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        IOSUiSettings(
+          title: 'Cropper',
         ),
       ],
-    ),
-  );
-}
-
-void _pickImages(ImageSource imageSource) {
-  _picker
-      .getImage(
-    source: imageSource,
-    imageQuality: 70,
-    maxHeight: 500,
-    maxWidth: 500,
-  )
-      .then((file) {
-    if (file != null) {
-      setState(() {
-        _imageFile = File(file.path);
-        check = '1';
-      });
-    }
-  });
-}
+    ).then((file) {
+      if (file != null) {
+        setState(() {
+          _imageFile = File(file.path);
+        });
+      }
+    });
+  }
 }
